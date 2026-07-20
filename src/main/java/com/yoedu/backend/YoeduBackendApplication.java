889d6d5eb27d585704badd5d7afac15a9349c838
vule2005718 <vule2005718@gmail.com>
2026-07-20 21:32:06 +0700

    chore: initialize Spring Boot project

---
+++ src/main/java/com/yoedu/backend/YoeduBackendApplication.java
@@ -0,0 +1,13 @@
+package com.yoedu.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YoeduBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YoeduBackendApplication.class, args);
+	}
+
+}
